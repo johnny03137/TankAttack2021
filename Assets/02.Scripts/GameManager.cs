ebca15b4da23d11c9e67d9e80b9139ed776be287
Heeyong Choi <johnny03137@gmail.com>
2021-04-27 16:37:44 +0900

    GameManager 1st done

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -8,9 +8,9 @@ public class GameManager : MonoBehaviour
 {
     private void Awake()
     {
-        Vector3 pos = new Vector3(Random.Range(-200.0f, 200.0f), 5.0f, Random.Range(-200.0f, 200.0f));
+        Vector3 pos = new Vector3(Random.Range(-150.0f, 150.0f), 5.0f, Random.Range(-150.0f, 150.0f));
 
         // 통신이 가능한 주인공 캐릭터(탱크)
-        PhotonNetwork.Instantiate("Tank", new Vector3(0f, 5.0f, 0f), Quaternion.identity,0 );
+        PhotonNetwork.Instantiate("Tank", pos, Quaternion.identity, 0 );
     }
 }
